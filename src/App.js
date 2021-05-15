de2ee958b89f340c35027d430695c9380777a444
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-05-15 11:18:16 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import HomePage from './page/home-page'
-
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <HomePage />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
